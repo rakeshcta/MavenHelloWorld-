83d6e3109f53aff5a7de129e8dd863a9acb2a036
rakeshcta <109577869+rakeshcta@users.noreply.github.com>
2022-07-21 13:18:43 +0530

    Update App.java

--- src/main/java/com/w3spoint/App.java
+++ src/main/java/com/w3spoint/App.java
@@ -1,1 +1,13 @@
-
+package com.w3spoint;
+ 
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
